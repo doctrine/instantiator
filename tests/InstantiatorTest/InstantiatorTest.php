35f1b2eccff023867cae0443c0bb4cc9ddb2e5c2
Marco Pivetta <ocramius@gmail.com>
2014-06-13 12:42:39 +0200

    Base tests for the `Instantiator` class

---
+++ tests/InstantiatorTest/InstantiatorTest.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace InstantiatorTest;
+
+use Instantiator\Instantiator;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Tests for {@see \Instantiator\Instantiator}
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ *
+ * @covers \Instantiator\Instantiator
+ */
+class InstantiatorTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Instantiator
+     */
+    private $instantiator;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->instantiator = new Instantiator();
+    }
+
+    /**
+     * @param string $className
+     *
+     * @dataProvider getInstantiableClasses
+     */
+    public function testCanInstantiate($className)
+    {
+        $this->assertInstanceOf($className, $this->instantiator->instantiate($className));
+    }
+
+    /**
+     * Provides a list of instantiable classes (existing)
+     *
+     * @return string[][]
+     */
+    public function getInstantiableClasses()
+    {
+        return array(
+            array('stdClass'),
+            array(__CLASS__),
+            array('Instantiator\\Instantiator'),
+            array('Phar'),
+        );
+    }
+}
