e3c28a61b552bfb698c8c88e30465998723053fc
Marco Pivetta <ocramius@gmail.com>
2014-06-13 15:35:51 +0200

    Installation should skip dev dependencies for 5.3.3

--- .travis.install.sh
+++ .travis.install.sh
@@ -3,9 +3,10 @@ if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm-nightly
     curl -sS https://getcomposer.org/installer > composer-installer.php
     hhvm composer-installer.php
     hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
-    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar install --dev --prefer-source
+elif [ "$TRAVIS_PHP_VERSION" = '5.3.3' ] ; then
+    composer self-update
+    composer update --prefer-source --no-dev
 else
     composer self-update
     composer update --prefer-source
-    composer install --dev --prefer-source
 fi
